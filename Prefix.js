254be09857e69f9b3e1ce5a3e34f64aaa9c18617
Hols <106111375+cnrsamet@users.noreply.github.com>
2024-06-28 23:47:07 +0300

    prefix

---
+++ Prefix.js
@@ -0,0 +1,17 @@
+const longestCommonPrefix = function(strs) {
+    if (strs.length === 0) {
+        return "";
+    }
+    let referenceString = strs[0];
+    for (let i = 0; i < referenceString.length; i++) {        
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] === undefined || referenceString[i] !== strs[j][i]) {
+                return referenceString.slice(0, i);
+            }
+        }
+    }
+    return referenceString;
+};
+
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
